ea2e62a3f61dc18e8f5ea460c31b1539a8fd6efb
CodeJunior06 <san1428.srp@gmail.com>
2022-04-16 03:22:00 -0300

    Implement layout and splash screen

--- app/src/main/java/com/codejunior/coinkrest/MainActivity.kt
+++ app/src/main/java/com/codejunior/coinkrest/MainActivity.kt
@@ -2,12 +2,43 @@ package com.codejunior.coinkrest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.codejunior.coinkrest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        Thread.sleep(1500)
-        setTheme(R.style.splash)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val nanController: NavController = Navigation.findNavController(View(applicationContext))
+        val arrayDocuments = resources.getStringArray(R.array.DocumentArray)
+        val arrayGender = resources.getStringArray(R.array.Gender)
+
+        val adapterDocuments = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_dropdown_item, arrayDocuments
+        )
+
+        val adapterGender = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_dropdown_item, arrayGender
+        )
+
+        binding?.spinnerDocument?.adapter = adapterDocuments
+        binding?.spinnerGender?.adapter = adapterGender
+
+        binding?.btnMain?.setOnClickListener{
+            nanController.navigate(R.id.action_global_viewData)
+        }
+
     }
 }
--- app/src/main/java/com/codejunior/coinkrest/Splash.kt
+++ app/src/main/java/com/codejunior/coinkrest/Splash.kt
@@ -1,12 +1,32 @@
 package com.codejunior.coinkrest
 
+import android.app.ActivityOptions
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        actionBar?.hide()
+       /* window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)*/
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.statusBarColor = ContextCompat.getColor(this,R.color.background_splash)
         setContentView(R.layout.activity_splash)
+
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left)
+            finish()
+        }, 2000)
+
     }
 }
